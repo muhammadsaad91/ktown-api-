94aaaa80f197289d2c037b8642dd112f76c2cd68
Muhammad SaAd <78014984+muhammadsaad91@users.noreply.github.com>
2023-04-02 02:28:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,26 @@ app.use(bodyParser.json());
 
 // Handle incoming webhook requests
 app.post('/webhooks/orders/create', (req, res) => {
-  console.log('Received webhook:', req.body);
+  const order = req.body;
+  console.log('Received webhook for order:', order);
+  
+  // Extract the relevant information from the order object
+  const orderId = order.id;
+  const customerEmail = order.email;
+  const lineItems = order.line_items;
+  const totalPrice = order.total_price;
+
+  // Do something with the extracted information, such as adding it to a database or sending a notification
+  // ...
+
   res.status(200).send('Webhook received successfully');
 });
+
+
+
+
+
+
 
 // Start the server
 app.listen(port, () => {
