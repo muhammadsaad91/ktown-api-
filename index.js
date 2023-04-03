e9a9c2328d2e76a9c2ba0ad6a9f0f22883512307
Muhammad SaAd <78014984+muhammadsaad91@users.noreply.github.com>
2023-04-03 16:21:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,46 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Use body-parser middleware to parse incoming webhook data
 app.use(bodyParser.json());
 
+// Handle incoming webhook requests
 app.post('/webhooks/orders/create', (req, res) => {
   console.log('Received webhook:', req.body);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   res.status(200).send('Webhook received successfully');
 });
 
